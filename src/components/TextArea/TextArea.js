ec1e497e72f324d38db420c997c5ab62d67f3636
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-08-27 19:09:20 +0530

    adding functionality of lowercase and cleartext

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -13,6 +13,12 @@ export default function TextArea(props) {
   const changeToUpperCase = () => {
     setText(text.toLocaleUpperCase());
   }
+  const changeToLowerCase =() => {
+    setText(text.toLocaleLowerCase());
+  }
+  const clearText = () => {
+    setText("");
+  }
   return (
     <div className="container">
         <Box
@@ -43,12 +49,12 @@ export default function TextArea(props) {
             </svg>
           UpperCase
           </button>
-          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+          <button onClick={changeToLowerCase}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M10 2a.75.75 0 01.75.75v12.59l1.95-2.1a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 111.1-1.02l1.95 2.1V2.75A.75.75 0 0110 2z" clipRule="evenodd" />
             </svg>
           LowerCase
           </button>
-          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+          <button onClick = {clearText}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clipRule="evenodd" />
             </svg>
           Clear
