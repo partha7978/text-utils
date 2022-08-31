0a316ac62096ec515c42f34443842ea9773294d6
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-08-31 18:54:22 +0530

    adding undo functionality

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -8,7 +8,6 @@ import Stack from '@mui/material/Stack';
 export default function TextArea(props) {
   //using state to store input value 
   const [text, setText] = useState('');
-
   //using state to store alert message
   const [alertStatus, setAlertStatus] = useState('none');
   const [aLertMsg, setALertMsg] = useState("");
@@ -28,11 +27,23 @@ export default function TextArea(props) {
   const changeToLowerCase =() => {
     setText(text.toLocaleLowerCase());
   }
-  const clearText = () => {
-    setText("");
-  }
   const capitalizeFirstLetter = () => {
     setText(text.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" "));
+  }
+  const clearText = () => {
+    window.saveText = text;
+    setText("");
+  }
+  const UndoText = () => {
+    if(window.saveText !== undefined) {
+      setText(window.saveText);
+      window.saveText = undefined;
+    }
+    else {
+      setText(text);
+      setALertMsg("Please enter some text / clear the text to undo");
+      setAlertStatus('block');
+    }
   }
   return (
     <div className="container">
@@ -69,13 +80,18 @@ export default function TextArea(props) {
             </svg>
           LowerCase
           </button>
+          <button onClick = {capitalizeFirstLetter}>
+            CapitalizeFirstLetter
+          </button>
           <button onClick = {clearText}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clipRule="evenodd" />
             </svg>
           Clear
           </button>
-          <button onClick = {capitalizeFirstLetter}>
-            CapitalizeFirstLetter
+          <button onClick = {UndoText}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+          Undo
           </button>
         </div>
         <div className="alertMsg">
