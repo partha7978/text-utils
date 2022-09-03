ba1e7c30ee8de9310dd9e20c792f8849eeefdc19
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-09-04 00:13:03 +0530

    copy to clipboard function added

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -86,6 +86,23 @@ export default function TextArea(props) {
       // setAlertType({alertStatus: 'block', alertMsg:"Please enter some text / clear the text to undo", alertType:"warning"});
     }
   }
+  // todo: copyToClipboard
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text);
+    if(text.length === 0) {
+      setAlertType({alertStatus: 'block', alertMsg:"No text found for copy", alertType:"error"});
+      setTimeout(() => {
+        setAlertType({alertStatus: 'none', alertMsg:"", alertType:"success"});
+      } , 1500);
+    }
+    else {
+      setAlertType({alertStatus: 'block', alertMsg:"Text copied to clipboard", alertType:"success"});
+      setTimeout(() => {
+        setAlertType({alertStatus: 'none', alertMsg:"", alertType:"success"});
+      } , 1500);
+    }
+  }
+   
   return (
     <div className="container">
         <Box
@@ -137,6 +154,12 @@ export default function TextArea(props) {
             </svg>
           Undo
           </button>
+          <button onClick = {copyToClipboard}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+            </svg>
+          Copy
+          </button>
+        
         </div>
         <div className="alertMsg">
           <div style={{display: alertType.alertStatus}}>
