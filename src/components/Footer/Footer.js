67dd67a9e06ddec6d68e1a667a96f4c4937b00cc
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-09-18 00:16:37 +0530

    styling the footer text and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,12 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <p>Created by <a href="https://parthasarathimuduli.netlify.app/" target='_self'>Partha</a></p>
+        <div className='footer-icons'>
+            <button><ion-icon name="logo-linkedin"></ion-icon></button>
+            <button><ion-icon name="logo-github"></ion-icon></button>
+        </div>
+    </div>
   )
 }
