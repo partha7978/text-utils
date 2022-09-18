cbc69cc8ebe46d48771742a33c3d363d4a65be49
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-09-19 00:27:05 +0530

    footer section completed and added a new color theme

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footer'>
-        <p>Created by <a href="https://parthasarathimuduli.netlify.app/" target='_self'>Partha</a></p>
+        <p>Created by <a rel="noreferrer" href="https://parthasarathimuduli.netlify.app/" target='_blank'>Partha</a></p>
         <div className='footer-icons'>
-            <button><ion-icon name="logo-linkedin"></ion-icon></button>
-            <button><ion-icon name="logo-github"></ion-icon></button>
+            <button> <a  rel="noreferrer" href="https://www.linkedin.com/in/partha-sarathi-muduli-1738921b9/" target='_blank'><ion-icon name="logo-linkedin"></ion-icon></a></button>
+            <button> <a  rel="noreferrer" href="https://github.com/partha7978" target='_blank'><ion-icon name="logo-github"></ion-icon></a></button>
         </div>
     </div>
   )
